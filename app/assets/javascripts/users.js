d943ced039ce9eb9b08f42a6042399c9e1898460
yomoda07 <kanji.yy@gmail.com>
2017-10-08 13:04:30 -0700

    Final virsion (Added jquery)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,45 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function() {
+  $("#1").on('click', function(e) {
+    e.preventDefault();
+    var checkingBalance = $('#checking-balance').text().split("$")[1];
+    updatedChecking = checkingBalance - 1
+    $('#checking-balance').text("$" + updatedChecking);
+    var winnings = $("#winnings").text();
+    $("#winnings").text((parseInt(winnings) + 1));
+    savingsBalance = $("#savings-balance").text().split("$")[1];
+    updatedSaving = savingsBalance - 1
+    // ('#checking-balance').text("$" + updatedSaving);
+    // savings-balance
+    // token-balance
+  });
+
+  $("#5").on('click', function(e) {
+    e.preventDefault();
+    var checkingBalance = $('#checking-balance').text().split("$")[1];
+    updatedChecking = checkingBalance - 5
+    $('#checking-balance').text("$" + updatedChecking)
+  });
+
+  $("#10").on('click', function(e) {
+    e.preventDefault();
+    var checkingBalance = $('#checking-balance').text().split("$")[1];
+    updatedChecking = checkingBalance - 10
+    $('#checking-balance').text("$" + updatedChecking)
+  });
+
+  $("#50").on('click', function(e) {
+    e.preventDefault();
+    var checkingBalance = $('#checking-balance').text().split("$")[1];
+    updatedChecking = checkingBalance - 50
+    $('#checking-balance').text("$" + updatedChecking)
+  });
+});
+//
+// checking-balance
+// winnings
+// savings-balance
+// token-balance
